a98a041f6e888690654601da8383195dd017256f
minorusso <gumbomb1@gmail.com>
2021-12-16 01:12:24 +0900

    profile画像投稿機能＆編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
   resources :sessions, only: %i[new create destroy]
   # newアクションに登録フォーム表示
-  resources :users, only: %i[new create show]
+  resources :users
 end
