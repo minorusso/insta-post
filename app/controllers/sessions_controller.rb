cd3ab989381839938bd548fb86941c2288d1811b
minorusso <gumbomb1@gmail.com>
2021-12-15 13:27:07 +0900

    ログイン＆ログアウト機能追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    # パスワードとメールアドレスの組み合わせが有効かどうか判定することです。
+    # この判定によって、有効ならログインする、有効でない場合は再入力させるという次の動作
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sessions, only: %i[new create destroy]
   # newアクションに登録フォーム表示
   resources :users, only: %i[new create show]
 end
