aaff00e3f69e8fdf85a6b560d7375a0a0817c9f0
minorusso <gumbomb1@gmail.com>
2021-12-17 02:00:36 +0900

    heroku open時のエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'users#new'
   resources :blogs do
     collection do
       post :confirm
     end
   end
   resources :sessions, only: %i[new create destroy]
-  # newアクションに登録フォーム表示
   resources :users
   resources :favorites
 
