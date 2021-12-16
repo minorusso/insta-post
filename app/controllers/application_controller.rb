e987d4b621af15279f8d0a92ea8d65f09dadeb14
minorusso <gumbomb1@gmail.com>
2021-12-16 22:50:55 +0900

    ログイン中のユーザー名をヘッダーに表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   before_action :login_required
+  before_action :set_current_user
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
 
   private
 
